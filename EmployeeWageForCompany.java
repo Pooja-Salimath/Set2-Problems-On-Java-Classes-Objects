1fa218d68e00ee39db8d6d4d2ae07dc2f399e222
Pooja Virupakshi Salimath <20911@emudhra.local>
2020-07-16 12:02:56 +0530

    Random Checking If Employee is Present or Absent

--- EmployeeWageForCompany.java
+++ EmployeeWageForCompany.java
@@ -2,6 +2,12 @@ class EmployeeWageForCompany
 {
 	public static void main(String[] args)
 	{
+		int IS_FULL_TIME = 1;
 		System.out.println("-----: Welcome to Employee Wage Computation :----");
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+		if (empCheck == IS_FULL_TIME)
+			System.out.println("Employee is Present");
+		else
+			System.out.println("Employee is Absent");
 	}
 }
